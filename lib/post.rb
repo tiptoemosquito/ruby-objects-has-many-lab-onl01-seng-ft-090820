f52c48e57d1240934fb1ed12855cef055e2b628f
Christina Howard <clhoward33@icloud.com>
2020-09-21 20:49:32 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,20 @@
 class Post
 
+  attr_accessor :name, :author
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    author.name if author
+  end
+
 end
