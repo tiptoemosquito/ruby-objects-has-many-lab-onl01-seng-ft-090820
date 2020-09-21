28f123a25fbc2f1bcfd1c5649c4f9279b97517ec
Christina Howard <clhoward33@icloud.com>
2020-09-21 20:31:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,14 +14,16 @@ class Artist
   end
 
   def songs
-
+    Song.all.select {|song| song.artist}
   end
 
   def add_song(song)
-    songs.artist = self
+    song.artist = self
   end
 
   def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
   end
 
   def self.song_count
