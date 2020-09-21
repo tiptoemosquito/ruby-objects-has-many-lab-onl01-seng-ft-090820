8489f8f46cc8d5811a3b6a6f4b359cb0b2414edc
Christina Howard <clhoward33@icloud.com>
2020-09-21 20:48:25 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,35 @@
 class Author
 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def posts
+    Post.all.select {|post| post.artist == self}
+  end
+
+  def add_song(post)
+    song.artist = self
+  end
+
+  def add_post_by_name(name)
+    post = Post.new(name)
+    song.artist = self
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+
+end
+
 end
