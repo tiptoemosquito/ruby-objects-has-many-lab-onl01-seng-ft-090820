74784d03be14a597000b7171348b15fa86fb68ad
Christina Howard <clhoward33@icloud.com>
2020-09-21 20:19:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,29 @@
 class Artist
 
-  attr_accessor :song 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all?
+    @@all?
+  end
+
+  def songs
+  end
+
+  def add_song
+  end
+
+  def add_song_by_name
+  end
+
+  def self.song_count
+  end
+  
 
 end
